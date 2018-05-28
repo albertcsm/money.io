c7dff4e4a2a92af436121a82add41fa7afc97c96
Albert Cheung <albertcsm@gmail.com>
2018-05-29 01:46:35 +0800

    remove backend

--- money-io-server/src/main/kotlin/io/money/server/BuddyRepository.kt
+++
@@ -1,7 +0,0 @@
-package io.money.server
-
-import org.springframework.data.repository.CrudRepository
-
-interface BuddyRepository : CrudRepository<Buddy, String> {
-    fun findByName(name: String): List<Buddy>
-}
